2cac3a2a469bdc3e004b8315e46a783eecc34439
Judy Chong <judyjchong@gmail.com>
2022-06-05 22:31:56 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://judychong:sp00kymuld3r@cluster0.dnbqu.mongodb.net/dogTracker?retryWrites=true&w=majority', 
+    'dbName': 'dogTracker'
+};
